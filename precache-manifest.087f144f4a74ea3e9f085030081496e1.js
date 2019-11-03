2209100f1c5e8859e574c790a1a222a1cc0c2946
chuan shi(haoyang gao) <ghy118432@alibaba-inc.com>
2019-11-04 00:37:23 +0800

    Updates

--- precache-manifest.087f144f4a74ea3e9f085030081496e1.js
+++ precache-manifest.087f144f4a74ea3e9f085030081496e1.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ca6f5863062e2cc1a160d30380990b1e",
+    "revision": "860241324332afe390fc4bf5b4da37a6",
     "url": "/BMW2SeriesConvertible/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/BMW2SeriesConvertible/static/css/2.653462cc.chunk.css"
   },
   {
-    "revision": "a2c2dc3001e6617b1ddf",
+    "revision": "b769b2238733142ff46e",
     "url": "/BMW2SeriesConvertible/static/css/main.bf2c2525.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/BMW2SeriesConvertible/static/js/2.703245e9.chunk.js"
   },
   {
-    "revision": "a2c2dc3001e6617b1ddf",
-    "url": "/BMW2SeriesConvertible/static/js/main.3f6321e5.chunk.js"
+    "revision": "b769b2238733142ff46e",
+    "url": "/BMW2SeriesConvertible/static/js/main.34026cd0.chunk.js"
   },
   {
     "revision": "2e429d63cd41dcca30c0",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/BMW2SeriesConvertible/precache-manifest.4944e17bba4eb7ab62df7be899da780c.js"
+  "/BMW2SeriesConvertible/precache-manifest.087f144f4a74ea3e9f085030081496e1.js"
 );
 
 self.addEventListener('message', (event) => {
